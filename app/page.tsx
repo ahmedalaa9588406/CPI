22cb562cec3fdaf40524168e1c065f644a732400
Ahmed9588406 <ak9588406@gmail.com>
2025-01-17 19:00:19 +0200

    enhancement

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function WelcomePage() {
         </div>
         
         <div className="flex flex-col justify-around gap-6">
-          <h1 className="text-lg sm:text-xl md:text-4xl font-jura font-bold">AI PAT</h1>
+          <h1 className="text-lg sm:text-xl md:text-4xl font-jura font-bold">AI-PAT</h1>
           <div className="">
             <Link href="/home" className="flex justify-center">
               <Button variant="outline" className="w-3/4 sm:w-2/3 rounded-full border-none text-lg sm:text-xl">Let&apos;s start</Button>
